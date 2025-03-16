7a40f28a50f10366cbe6a850ddc27ec0849b73f1
Manish652 <manishbhunia455@gmail.com>
2025-03-16 19:27:14 +0530

    Initial commit - Added project files

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+       
+             Hi, I'm Manish Bhunia, a tech enthusiast and thinker, constantly exploring the vast world of technology. With a background in web development, cybersecurity, and backend technologies, I am passionate about future-proof tech fields and always eager to learn and adapt.
+I'm currently in my second year of college, pursuing a tech-focused education while independently diving into modern fields like cybersecurity and cloud computing. Over the years, I have explored various domains, including web development (Django, HTML, CSS, JavaScript, Bootstrap), app development, AI, and cybersecurity. My journey has helped me understand the evolving tech landscape and refine my focus toward a future-proof career path.
+
+I believe in continuous learning and challenging the status quo. While my college curriculum is outdated, I take the initiative to stay ahead by acquiring skills relevant to the industry. My interest lies in backend development, cybersecurity, and exploring how AI is shaping the future.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
